7e9c818bd698ba9954fbee600e2a1cde279b84fc
Simon Swain <simon@modus.com.au>
2013-06-29 17:20:48 +1000

    Added params

--- config/config.sample.js
+++ config/config.sample.js
@@ -1,3 +1,13 @@
+// how many ids to create
+var samples = 10;
+
+var inject = {
+  // how many samples to inject per second
+  rate: 100,
+  // how many seconds to inject for
+  duration: 60
+}
+
 var redis = {
   host: '127.0.0.1',
   port: 6379
@@ -17,7 +27,8 @@ var db = {
   url: 'postgres://' + pg.username + ':' + pg.password + '@' + pg.host + '/' + pg.database
 };
 
+exports.sampleCount = samples;
+exports.inject = inject;
 exports.prefix = 'stress';
-exports.sampleCount = 10;
 exports.db = db;
 exports.redis = redis;
